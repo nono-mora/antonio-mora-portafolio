42918ed2d7fa3f1f75098be26339fd97e4f6d981
Antonio Mora <amorab@ucenfotec.ac.cr>
2025-08-08 18:38:42 -0600

    Remove unused URLs from sitemap and add Google site verification file

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -6,23 +6,5 @@ export default function sitemap() {
       changeFrequency: "monthly",
       priority: 1,
     },
-    {
-      url: "https://antoniomora.dev#about",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://antoniomora.dev#projects",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: "https://antoniomora.dev#contact",
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.6,
-    },
   ];
 }
